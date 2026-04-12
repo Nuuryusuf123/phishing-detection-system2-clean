cef63ce5040eb10d33033ec28b98da96ca707a5f
Nur Mohamed Yusuf <nuurmaxamedyuusuf43@gmail.com>
2026-04-12 16:51:56 +0300

    Remove unnecessary newline in db.py

--- utils/db.py
+++ utils/db.py
@@ -11,7 +11,6 @@ DB = Path("data/app.db")
 def hash_password(password: str) -> str:
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
 
-
 def verify_password(password: str, hashed_password: str) -> bool:
     return bcrypt.checkpw(password.encode(), hashed_password.encode())
 
